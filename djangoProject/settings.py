9ef271e7b953cf6175774978307ec328324c35a3
Lucas Mazzo <mazzolucas0@gmail.com>
2023-10-11 01:39:38 +0000

    10/10/23

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-!hs1s(4k+^rj(iidzdv97f6s=0oh%ix_8qw*=vx9aa$7dfb5ly
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '8000-surershelf-pi2-ilq13oa703u.ws-us105.gitpod.io'
+]
 
 
 # Application definition
@@ -118,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- login/views.py
+++ login/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def Login(request):
     return render(request, 'Login.html')
+
+
+def Register(request):
+    return render(request, 'Register.html')
+
+
+def pagina_principal(request):
+    return render(request, 'Home.html')
